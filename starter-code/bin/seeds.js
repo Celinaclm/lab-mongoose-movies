3719afd1bf5b439c9154c02308c0d6de1e297bc7
Celinaclm <celinaclm@gmail.com>
2021-04-18 13:00:37 +0200

    iteration 0

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Celebrity = require('../models/Celebrity.js');
+
+const celebrities = [
+    {
+        name: 'Rachel McAdams',
+        occupation: 'actress',
+        catchPhrase: 'I drink maple syrup'
+    },
+    {
+        name: 'Will Ferrel',
+        occupation: 'actor',
+        catchPhrase: 'YOU SIT ON A THRONE OF LIES!'
+    },
+    {
+        name: 'Dan Stevens',
+        occupation: 'actor',
+        catchPhrase: 'The comfort zone is the great enemy to creativity'
+    }
+];
+
+
+
